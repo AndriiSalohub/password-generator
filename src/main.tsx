34985dc35a1e307d28d5b39a984c891cba5de9cc
Andrii <andrejsalogub3@gmail.com>
2023-08-04 12:13:33 +0300

    added styles and markup for password input component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { App } from "./containter/App.tsx";
+import { App } from "./containter/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
