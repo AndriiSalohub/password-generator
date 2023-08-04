00fb5cbb84424ee98be15c30ad5a0380aa97c6fa
Andrii <andrejsalogub3@gmail.com>
2023-08-04 22:26:52 +0300

    added the functionality of checking the received password for complexity

--- src/components/GenerateBtn.tsx
+++ src/components/GenerateBtn.tsx
@@ -69,7 +69,10 @@ export const GenerateBtn: FC = () => {
         <button
             className="w-full bg-succsess border border-primary mt-5 p-3 font-primary hover:border  hover:bg-primary hover:text-secondary ease-in-out duration-300 hover:duration-300 hover:ease-in-out"
             onClick={() => {
-                handlePassword();
+                {
+                    handlePassword();
+                    checkStrength();
+                }
             }}
         >
             Generate
--- src/components/PasswordStrength.tsx
+++ src/components/PasswordStrength.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
+import { useStore } from "../store/store";
 
 export const PasswordStrength: FC = () => {
+    const { strength } = useStore();
+
     return (
         <div className="bg-primary flex justify-between mt-5 p-4">
             <h2 className="font-secondary text-gray-600 text-md">Strength</h2>
@@ -10,6 +13,13 @@ export const PasswordStrength: FC = () => {
                         type="checkbox"
                         className="appearance-none w-2 h-5 border checked:bg-strength"
                         disabled
+                        checked={
+                            strength === "weak" ||
+                            strength === "medium" ||
+                            strength === "strong"
+                                ? true
+                                : false
+                        }
                     />
                 </li>
                 <li>
@@ -17,6 +27,11 @@ export const PasswordStrength: FC = () => {
                         type="checkbox"
                         className="appearance-none w-2 h-5 border checked:bg-strength"
                         disabled
+                        checked={
+                            strength === "medium" || strength === "strong"
+                                ? true
+                                : false
+                        }
                     />
                 </li>
                 <li>
@@ -24,13 +39,7 @@ export const PasswordStrength: FC = () => {
                         type="checkbox"
                         className="appearance-none w-2 h-5 border checked:bg-strength"
                         disabled
-                    />
-                </li>
-                <li>
-                    <input
-                        type="checkbox"
-                        className="appearance-none w-2 h-5 border checked:bg-strength"
-                        disabled
+                        checked={strength === "strong" ? true : false}
                     />
                 </li>
             </ul>
